a337e087d9198772dc405a601632fa98fadfe588
Sriram Varma Manthina <smanthina@zetaglobal.com>
2025-05-14 15:32:02 +0530

    Added User Model

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+# fields added in User Table
+# email:string
+# password_digest:string
+
+# fields added by has_secure_password
+# password:string virtual
+# password_confirmation:string virtual
+
+class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+end
