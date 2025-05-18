38ed8e3ae643c97a369f40161531cacf997648cd
Sriram Varma Manthina <smanthina@zetaglobal.com>
2025-05-18 18:00:15 +0530

    adding user sign-ups

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,6 @@
 class MainController < ApplicationController
   def index
-    flash[:notice] = "Logged in Successfully"
-    flash[:alert] = "Invalid email or password"
+    # flash[:notice] = "Logged in Successfully"
+    # flash[:alert] = "Invalid email or password"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   root "main#index"
 
   # GET /about (goes to about controller, index action)
-  get "about-us", to: "about#index", as: :about
-
+  get "about_us", to: "about#index", as: :about
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
 end
